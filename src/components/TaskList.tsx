2abff12ad67320320672f73bf0e5c644d6f727d3
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-05-30 10:24:49 -0300

    Ajuste do placeholder alterado para o padrão do Jest

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -53,7 +53,7 @@ export function TaskList() {
         <div className="input-group">
           <input
             type="text"
-            placeholder="Adicionar nova Tarefa"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
